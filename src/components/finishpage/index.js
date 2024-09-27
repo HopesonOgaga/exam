bfecf1ccf45c5056c25af9b5552b23823bde298a
hopeson <hopesonogaga@gmail.com>
2024-09-28 00:02:34 +0100

    ui

---
+++ src/components/finishpage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Finished() {
+  return (
+    <section>
+      <div className="w-full h-screen flex justify-center items-center gap-8 flex-col md:flex-row">
+      <div className=""><img className="w-32 h-32" src="/image/yes.png" alt=""></img></div>
+      <div className=" flex flex-col gap-4 ">
+        <p className="capitalize font-semibold text-xl text-center md:text-left ">you Finished your exam</p>
+        <p className="capitalize font-thin text-sm text-center md:text-left ">your admin will release your results shortly, all the best</p>
+      </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/header/finishedheader.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function FinishedHeader() {
+  return (
+    <section>
+      <div>
+        ,<p className="">proto x</p>
+      </div>
+      <div>
+        <img src="/image/right-arrow.png" alt=""></img>
+        <button className="">home</button>
+      </div>
+    </section>
+  );
+}
